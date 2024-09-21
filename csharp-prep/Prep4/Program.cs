8413b5ec86e8256370008b18bc58079481cd0418
zackadoodledoo <sin12014@byui.edu>
2024-09-20 21:18:29 -0700

    update 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,7 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name? ");
-        string first = Console.ReadLine();
+        List<int> numbers = GetUserNumbers();
+
+        int sum = ComputeSum(numbers);
+        Console.WriteLine($"The sum is: {sum} ");
+
+        float average = ComputeAverage(sum, numbers.Count);
+        Console.WriteLine($"The average is: {average} ");
+
+        int max = FindMax(numbers);
+        Console.WriteLine($"The max is: {max} ");
+
+    }
+
+    static List<int> GetUserNumbers()
+    {
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+
+        while (userNumber !=0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber !=0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        return numbers;
+    }
+    
+    static int ComputeSum(List<int> numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+
+    static float ComputeAverage(int sum, int count)
+    {
+        return ((float)sum) / count;
+    }
+
+    static int FindMax(List<int> numbers)
+    {
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        return max;
     }
 }
