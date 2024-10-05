84b4382f9e9a077c17e141cf50b979edaf5b35a4
zackadoodledoo <sin12014@byui.edu>
2024-10-04 22:51:04 -0700

    Scripture 1.0 done

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,5 +1,37 @@
 using System;
 
+namespace DailyScripture
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            Scripture scripture = new Scripture ("3 Nephi ", 5, 13, "Behold, I am a disciple of Jesus Christ, the Son of God. Ihave been called of him to declare his word among his people, that they might have everlasting life. ");
+            Console.Clear();
+            scripture.Display();
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to exit. ");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                    break;
+                    
+                if (!scripture.HideRandomWord())
+                {
+                    Console.WriteLine("\nYou got this! You memorized the scripture!");
+                    break;
+                }
+
+                Console.Clear();
+                scripture.Display();
+                Console.WriteLine("\nPress Enter to continue or type 'quit' to exit. ");
+
+            }
+        }
+    }
+}
 class Program
 {
     static void Main(string[] args)
