a8de24b020cbaed0a6a1d3a9af1591eda61fe649
zackadoodledoo <sin12014@byui.edu>
2024-10-22 21:56:06 -0700

    Eternal Quest 1.0

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -1,9 +1,90 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop06 World!");
+        string menuSelected = "";
+
+        GoalsTracker goals = new GoalsTracker();
+    
+    while (menuSelected != "6") {
+
+        int points = goals.GetAccumulatedPoints();
+
+        Console.WriteLine($"You have {points} points. ");
+        Console.WriteLine();
+        Console.WriteLine("Menu Options: ");
+        Console.WriteLine(" 1. Create New Goal");
+        Console.WriteLine(" 2. List Goals");
+        Console.WriteLine(" 3. Save Goals");
+        Console.WriteLine(" 4. Load Goals");
+        Console.WriteLine(" 5. Record Event");
+        Console.WriteLine(" 6. Quit");
+        Console.WriteLine("Select a choice from the menu: ");
+
+        menuSelected = Console.ReadLine();
+
+        switch (menuSelected) {
+            case "1":
+                Console.WriteLine("The Types of Goals are: ");
+                Console.WriteLine(" 1. Simple Goal");
+                Console.WriteLine(" 2. Eternal Goal");
+                Console.WriteLine(" 3. Checklist Goal");
+                Console.WriteLine("Which type of goal would you like to create? ");
+                string goalType = Console.ReadLine();
+
+                switch (goalType) {
+                    case "1":
+                        SimpleGoal newSimpleGoal = new SimpleGoal();
+                        newSimpleGoal.CreateChildGoal();
+                        goals.addGoal(newSimpleGoal);
+                        break;
+
+                    case "2":
+                        EternalGoal newEternalGoal = new EternalGoal();
+                        newEternalGoal.CreateChildGoal();
+                        goals.addGoal(newEternalGoal);
+                        break;
+
+                    case "3":
+                        ChecklistGoal newChecklistGoal = new ChecklistGoal();
+                        newChecklistGoal.CreateChildGoal();
+                        goals.addGoal(newChecklistGoal);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid goal type. Please try again. ");
+                        break;
+
+                }
+                break;
+
+            case "2":
+                goals.ListGoals();
+                break;
+
+            case "3":
+                goals.SaveGoals();
+                break;
+
+            case "4":
+                goals.LoadGoals();
+                break;
+
+            case "5":
+                goals.RecordEventInTracker();
+                break;
+
+            case "6":
+                Environment.Exit(0);
+                break;
+
+            default:
+                Console.WriteLine("Please select a valid number from the menu options. ");
+                break;
+            }
+        }
     }
 }
