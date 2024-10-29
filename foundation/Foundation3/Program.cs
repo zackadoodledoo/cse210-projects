ee2a4e38c305943ebc16efd621bd1edcc8a1771f
zackadoodledoo <sin12014@byui.edu>
2024-10-28 21:27:00 -0700

    exercise program 2.0

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        List<Exercise> exerciseList = new List<Exercise>();
+
+        Running running = new Running("October 28, 2024", 30, 4.8);
+        exerciseList.Add(running);
+
+        StationaryBicycles stationaryBicycles = new StationaryBicycles("October 29, 2024", 60, 15);
+        exerciseList.Add(stationaryBicycles);
+
+        Swimming swimming = new Swimming("October 30, 2024", 20, 20);
+        exerciseList.Add(swimming);
+
+        Console.WriteLine("Exercises:" );
+        Console.WriteLine();
+
+        foreach (Exercise exercise in exerciseList)
+        {
+            Console.WriteLine(exercise.GetSummary());
+        }
     }
 }
